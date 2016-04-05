4bfa87eb90b791c910e95d6cfa4b3342d4859a5f
chetansavdekar <chetan.savdekar@gmail.com>
2016-04-05 20:00:27 +0530

    Initial commit

---
+++ WAAD.POC.ProductCatalog.Backend/Controllers/NotificationController.cs
@@ -0,0 +1,40 @@
+﻿using System.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.NotificationHubs;
+
+namespace WAAD.POC.ProductCatalog.Backend.Controllers
+{
+    public class NotificationController : ApiController
+    {
+        // POST api/notification
+        public async Task<HttpResponseMessage> Get([FromUri]string message)
+        {
+            //string payloadMessage = String.Format(@"<toast><visual><binding template=""ToastText01""><text id=""1"">{0}</text></binding></visual></toast>", message);
+            string payloadMessage = String.Format(@"<toast launch=""1""><visual><binding template=""ToastGeneric""> 
+                                                  <text id=""1"">Dear customer, Get {0} if you come visit us today ! Best Regards from our team.</text> 
+                                                  </binding></visual> 
+                                                  <actions> 
+                                                    <action activationType=""foreground"" content=""Yes I will !"" arguments=""Yes""/> 
+                                                    <action activationType=""background"" content=""No thanks."" arguments=""No""/> 
+                                                  </actions> 
+                                                  </toast>", message);
+
+            var connectionString = ConfigurationManager.AppSettings["NotificationHubConnectionString"];
+            var notificationHubName = ConfigurationManager.AppSettings["NotificationHubName"];
+            NotificationHubClient hub = NotificationHubClient.CreateClientFromConnectionString(connectionString, notificationHubName);
+            var result = await hub.SendWindowsNativeNotificationAsync(payloadMessage);
+
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent("Success");
+            return response;
+        }
+
+    }
+}
---
+++ WAAD.POC.ProductCatalog.Backend/Controllers/RegisterDeviceController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Configuration;
+using System.IO;
+using Microsoft.Azure.NotificationHubs;
+using System.Text;
+
+namespace WAAD.POC.ProductCatalog.Backend.Controllers
+{
+    public class RegisterDeviceController : ApiController
+    {
+
+        // POST api/RegisterDevice
+        public void Post([FromBody] string value)
+        {
+            var connectionString = ConfigurationManager.AppSettings["NotificationHubConnectionString"];
+            var notificationHubName = ConfigurationManager.AppSettings["NotificationHubName"];
+            NotificationHubClient hub = NotificationHubClient.CreateClientFromConnectionString(connectionString, notificationHubName);
+            // Decode from Base64 
+            string Base64DecodedChannelUri = Encoding.UTF8.GetString(Convert.FromBase64String(value));
+            hub.DeleteRegistrationsByChannelAsync(Base64DecodedChannelUri).Wait();
+            hub.CreateWindowsNativeRegistrationAsync(Base64DecodedChannelUri);
+
+        }
+        
+    }
+}
